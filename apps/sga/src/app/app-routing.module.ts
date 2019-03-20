21cdac67204713fa6fb34434233d38d021baba32
-Eu- <eulier1@gmail.com>
2019-03-20 12:17:41 -0400

    set sidebar logic feature

--- apps/sga/src/app/app-routing.module.ts
+++ apps/sga/src/app/app-routing.module.ts
@@ -2,8 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+  {
+    path: 'list',
+    loadChildren: './list/list.module#ListPageModule'
+  },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
 ];
 
