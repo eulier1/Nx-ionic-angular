38d329b78c204db81ace9dce173222d503d2e202
-Eu- <eulier1@gmail.com>
2019-03-20 12:32:21 -0400

    set sidebar logic feature

--- apps/mobile/src/app/app.component.ts
+++ apps/mobile/src/app/app.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  Router,
+  NavigationStart,
+  ResolveStart,
+  ResolveEnd
+} from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
 
-import { Platform } from '@ionic/angular';
+import { Platform, MenuController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -8,7 +16,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
     {
       title: 'Home',
@@ -19,21 +27,65 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'Logout',
+      url: '/login',
+      icon: 'log-out'
     }
   ];
+
+  navStart: Observable<NavigationStart>;
+  navResStart: Observable<ResolveStart>;
+  navResEnd: Observable<ResolveEnd>;
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router,
+    private menu: MenuController
   ) {
     this.initializeApp();
+    this.menu.enable(true, 'sidebar');
+    // Create a new Observable that publish
+    // NavigationStart, ResolveStart, ResolveEnd
+    this.navStart = router.events.pipe(
+      filter(evt => evt instanceof NavigationStart)
+    ) as Observable<NavigationStart>;
+    this.navResStart = router.events.pipe(
+      filter(evt => evt instanceof ResolveStart)
+    ) as Observable<ResolveStart>;
+    this.navResEnd = router.events.pipe(
+      filter(evt => evt instanceof ResolveEnd)
+    ) as Observable<ResolveEnd>;
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+    });
+  }
+
+  ngOnInit() {
+    this.navStart.subscribe(evt => {
+      console.log('navStart', evt);
+      if (evt.url === '/login') {
+        this.menu.enable(false, 'sidebar');
+      }
+    });
+    this.navResStart.subscribe(evt => {
+      console.log('navResStart', evt);
+      if (evt.url === '/login') {
+        this.menu.enable(false, 'sidebar');
+      }
+    });
+    this.navResEnd.subscribe(evt => {
+      console.log('navResEnd', evt);
+      if (evt.url !== '/login') {
+        this.menu.enable(true, 'sidebar');
+      }
     });
   }
 }
