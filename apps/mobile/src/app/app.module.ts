516928cd858396d9128a1337631e386c7ef66c75
-Eu- <eulier1@gmail.com>
2019-03-24 22:52:38 -0400

    set single source API & ENV files

--- apps/mobile/src/app/app.module.ts
+++ apps/mobile/src/app/app.module.ts
@@ -9,18 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { LoginService } from './api/login.service';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    LoginService
   ],
   bootstrap: [AppComponent]
 })
--- apps/mobile/src/app/login/login.page.ts
+++ apps/mobile/src/app/login/login.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoginService } from '../api/login.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  constructor(private loginService: LoginService) {}
 
   ngOnInit() {
+    console.log(this.loginService.getAPI());
   }
-
 }
